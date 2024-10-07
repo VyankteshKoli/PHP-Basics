0aa387b80b5326ff80b207a79c8bca7e0799d8bb
Vyanktesh <141150425+VyankteshKoli@users.noreply.github.com>
2024-10-07 19:22:25 +0530

    Update c2.php

--- Coloning/c2.php
+++ Coloning/c2.php
@@ -1,15 +1,15 @@
 <?php
-class satyam
+class vyank
 {
     public $a;
 
 }
-class yash extends satyam
+class ram extends vyank
 {
 public $b;
 }
 
-$o=new satyam();
+$o=new vyank();
 $copy=clone $o;
 
 echo "set the value to the class member";
@@ -17,18 +17,12 @@ $o->a=12;
 $o->b=20;
 
 echo "set the value to the clone object";
-
-$copy->a="satyam"."<br>";
-$copy->b="yash"."<br>";
-
+$copy->a="vyank"."<br>";
+$copy->b="ram"."<br>";
 echo "print the value of class object<br>";
 echo $o->a;
 echo $o->b;
-
 echo "print the value of copy cloning <br>";
-
 echo $copy->a;
 echo $copy->b;
-
-
 ?>
