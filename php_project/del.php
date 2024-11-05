419043724c196f3fcc7f0ef06d3e1699e07cc622
Vyanktesh <141150425+VyankteshKoli@users.noreply.github.com>
2024-11-05 23:24:04 +0530

    Update del.php

--- php_project/del.php
+++ php_project/del.php
@@ -2,7 +2,7 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
-$dbName = "ashish";
+$dbName = "vyanktesh";
 $tableName = "college";
 
 $conn = mysqli_connect($serverName, $userName, $password, $dbName);
